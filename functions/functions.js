68d766cc4ac8cb75dbec10d851f91c0513353d74
LazarevicV <vukadinlazarevic@gmail.com>
2023-01-22 01:22:14 +0100

    LocalStorage saving values fixed

--- functions/functions.js
+++ functions/functions.js
@@ -1,5 +1,21 @@
+note_array = []
+
+if (localStorage.getItem('notes') === null) {
+  console.log('Localstorage je prazan!');
+}
+
+if (localStorage.getItem('notes') != null) {
+  note_array = JSON.parse(localStorage.getItem('notes'))
+}
+
+console.log(note_array);
+
 upisi_u_local_storage = (title, description, date, important) => {
-  localStorage.setItem('note', JSON.stringify([title, description, date, important]))
+  // console.log(title, description, date, important)
+  note = [title, description, date, important]
+  console.log(note);
+  note_array.push(note)
+  localStorage.setItem('notes', JSON.stringify(note_array))
 }
 
 ucitaj_podatke = () => {
@@ -8,9 +24,9 @@ ucitaj_podatke = () => {
   date = document.getElementById('date')
   important = document.getElementById('important')
   
-  console.log(title.value, description.value, date.value, important.value);
+  // console.log(title.value, description.value, date.value, important.value);
 
-  upisi_u_local_storage(title, description, date, important)
+  upisi_u_local_storage(title.value, description.value, date.value, important.value)
 }
 
 dodaj_dugme = document.getElementById('dodaj_dugme')
@@ -18,4 +34,5 @@ dodaj_dugme = document.getElementById('dodaj_dugme')
 dodaj_dugme.addEventListener('click', function dodaj_dugme_klik(e) {
   e.preventDefault()
   ucitaj_podatke()
+  location.reload()
 })
